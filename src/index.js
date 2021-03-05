ac33017b4c5399cd30a0dbc9c30aec0a4ab39fe1
Atul Verma <60815169+atul2861@users.noreply.github.com>
2021-03-05 15:30:41 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,153 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const port = 3000
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+// your code goes here
+
+app.get("/",(req,res)=>{
+    res.send("Hello world!");
+});
+
+app.post("/add",(req,res)=>{
+    const num1=Number(req.body.num1);
+    //console.log(typeof req.body.num1);
+    const num2=Number(req.body.num2);
+    const result=num1+num2;
+    if(num1 < -1000000 || num2 < -1000000 || result < -1000000){
+        res.send({
+            status:"error",
+            message:"Underflow",     
+            });
+    }
+    else if(num1 > 1000000 || num2>1000000 || result > 1000000){
+        res.send({
+            status:"error",
+            message:"Overflow",           
+            });
+    }
+    else if(isNaN(req.body.num1) || isNaN(req.body.num2) ){
+    
+        res.send({
+            status:"error",
+            message:"Invalid data types",           
+            });
+    }
+    else{
+        res.send({
+            status: "success",
+            message: "the sum of given two numbers",
+            sum: result,
+            });
+    }
+});
+ app.post("/sub",(req,res)=>{
+    const num1=Number(req.body.num1);
+    const num2=Number(req.body.num2);
+     const result=num1-num2;
+     if(num1 < -1000000 || num2 < -1000000 || result < -1000000){
+         res.send({
+             status:"error",
+             message:"Underflow",           
+             });
+     }
+     else if(num1 > 1000000 || num2>1000000 || result > 1000000){
+        res.send({
+            status:"error",
+            message:"Overflow",           
+            });
+    }
+     else if(isNaN(req.body.num1) || isNaN(req.body.num2)){
+        res.send({
+             status:"error",
+             message:"Invalid data types",           
+             });
+     }
+     else{
+         res.send({
+             status: "success",
+             message: "the difference of given two numbers",
+             difference: result,
+             });
+     }
+ });
+ app.post("/multiply",(req,res)=>{
+    const num1=Number(req.body.num1);
+    const num2=Number(req.body.num2);
+     const result=num1*num2;
+    if(num1 < -1000000 || num2 < -1000000 || result < -1000000){
+         res.send({
+             status:"error",
+             message:"Underflow",           
+             });
+     }
+     else if(num1 > 1000000 || num2>1000000 || result > 1000000){
+        res.send({
+            status:"error",
+            message:"Overflow",           
+            });
+    }
+     else if(isNaN(req.body.num1) || isNaN(req.body.num2)){
+    
+         res.send({
+             status:"error",
+             message:"Invalid data types",          
+             });
+     }
+     else{
+         res.send({
+             status: "success",
+             message: "The product of given numbers",
+             result: result,
+             });
+     }
+ });
+ app.post("/divide",(req,res)=>{
+    const num1=Number(req.body.num1);
+    const num2=Number(req.body.num2);
+   
+     const result=num1/num2;
+     if(num2===0){
+        res.send({
+            status:"error",
+            message:"Cannot divide by zero"
+        });
+    }
+     else if(num1 < -1000000 || num2 < -1000000 || result < -1000000){
+         res.send({
+             status:"error",
+             message:"Underflow",          
+             });
+     }
+     else if(num1 > 1000000 || num2>1000000 || result > 1000000){
+        res.send({
+            status:"error",
+            message:"Overflow",           
+            });
+    }
+    else if(isNaN(req.body.num1) || isNaN(req.body.num2)){
+    
+         res.send({
+             status:"error",
+             message:"Invalid data types",           
+             });
+     }
+    else{
+         res.send({
+             status: "success",
+             message: "The division of given numbers",
+             result: result,
+             });
+     }
+ });
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;
